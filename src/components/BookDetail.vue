0e6947dc4c5095e1fc0cbcc3e14a231b10f81060
Your Name <you@example.com>
2022-08-26 00:35:24 +0900

    動的パラメータ

---
+++ src/components/BookDetail.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>本の詳細</p>
+    <p>タイトル：{{ title }}</p>
+    <p>コンテンツ：{{ content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: Number,
+    title: String,
+    content: String,
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-  </div>
+    <button @click="checkRouteRouter">route and router</button>
+    <button @click="gotoHome">Homeにもどる</button>
+</div>
 </template>
+
+<script>
+export default {
+  methods: {
+    checkRouteRouter() {
+      console.log('---this.$route---');
+      console.log(this.$route);
+      console.log('---this.$router--');
+      console.log(this.$router);
+    },
+    gotoHome() {
+      this.$router.push('/');
+    }
+  }
+}
+</script>
